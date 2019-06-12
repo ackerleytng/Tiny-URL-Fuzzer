30ec007485db898c1f95f68d658f32897a55d2c8
Ackerley Tng <ackerleytng@gmail.com>
2019-06-13 07:04:50 +0800

    Update self_test.py to allow user to specify test target

--- self_test.py
+++ self_test.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python
 
+import sys
 from util import execute
 from const import PARSERS, REQUESTERS
+
+
+test_target = "http://11.11.11.11"
+if len(sys.argv) > 1:
+    test_target = sys.argv[1]
 
 
 for lang_library, binary in PARSERS.iteritems():
     lang, library = lang_library.split(".", 1)
     print(lang_library)
-    print(execute(lang, binary, "http://11.11.11.11", "bin/parser/"))
+    print(execute(lang, binary, test_target, "bin/parser/"))
 
 
 for lang_library, binary in REQUESTERS.iteritems():
     lang, library = lang_library.split(".", 1)
     print(lang_library)
-    print(execute(lang, binary, "http://11.11.11.11", "bin/requester/"))
+    print(execute(lang, binary, test_target, "bin/requester/"))
